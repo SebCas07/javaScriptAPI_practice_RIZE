a8078d25ee8683343c6a545d49dd3917b5ddfc37
SebCas07 <tiancas07@gmail.com>
2026-03-29 21:06:29 -0500

    getGiphy function setup

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@ console.log("script.js loaded");
 let data = fetch("https://api.giphy.com/v1/gifs/search?api_key=U7Kalu7XRiJmzVTKDjVWzWRWjTUNWrrb&q=&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips");
 
 console.log(data); 
+
+async function getGiphy () { 
+    const response = await fetch("https://api.giphy.com/v1/gifs/search?api_key=U7Kalu7XRiJmzVTKDjVWzWRWjTUNWrrb&q=&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips");
+    const data = await response.json(); 
+    const fact = data.facts[0]; 
+
+    const output = document.getElementById("gif-contanier");
+    output.textContent = fact;
+}
+
+getGiphy()
