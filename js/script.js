2dce7d56fbf8217def37336d66820dd87ced9269
SebCas07 <tiancas07@gmail.com>
2026-03-29 20:55:42 -0500

    API key is now active

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
 console.log("script.js loaded");
+
+let data = fetch("https://api.giphy.com/v1/gifs/search?api_key=U7Kalu7XRiJmzVTKDjVWzWRWjTUNWrrb&q=&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips");
+
+console.log(data); 
