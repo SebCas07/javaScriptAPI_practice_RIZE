638c910130164e915714d04c4ace9f0598e9bde2
SebCas07 <tiancas07@gmail.com>
2026-03-29 21:48:21 -0500

    applied loop

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 console.log("script.js loaded");
 
-let data = fetch("https://api.giphy.com/v1/gifs/search?api_key=U7Kalu7XRiJmzVTKDjVWzWRWjTUNWrrb&q=&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips");
+const gifContainer = document.querySelector("#gif-container")
+const gifBtn = document.querySelector("#fetch-gif-btn")
+const searchInput = document.querySelector("#search-input")
 
-console.log(data); 
+gifBtn.addEventListener("click", async function(){
+    // grab what user typed
+    const query = searchInput.value 
 
-async function getGiphy () { 
-    const response = await fetch("https://api.giphy.com/v1/gifs/search?api_key=U7Kalu7XRiJmzVTKDjVWzWRWjTUNWrrb&q=&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips");
+    const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=U7Kalu7XRiJmzVTKDjVWzWRWjTUNWrrb&q=${query}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`);
     const data = await response.json(); 
-    const fact = data.facts[0]; 
+    const images = data.data.map(gif => gif.images.original.url);
 
-    const output = document.getElementById("gif-contanier");
-    output.textContent = fact;
-}
-
-getGiphy()
+    // clear previous results first 
+    gifContainer.innerHTML="" 
+    for(let i = 0; i < images.length; i++){
+        gifContainer.innerHTML += `<img src="${images[i]}" class="col-3 mb-3">`;
+    } 
+    getGiphy()
+})
